06e02f2240366386d3be6bd566e0c934cddec305
Leonardo Viana <leonardo.viana@meep-app.com>
2020-06-21 17:53:01 -0300

    Definindo dashboard e estrutura da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    DashboardComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Vehicle } from '../interfaces/vehicle';
+import { VehicleService } from '../hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  constructor(private vehicleService: VehicleService) { }
+
+  vehicles: Vehicle[] = [];
+
+   ngOnInit() {
+    this.getVehicles();
+    console.log(this.vehicles);
+  }
+
+  getVehicles(): void {
+    this.vehicleService.getVehicles()
+      .subscribe(vehicles => this.vehicles = vehicles);
+  }
+
+}
